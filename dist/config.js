ae1ab616a2990a1a13329bc191b6d7ce7a5143a9
codegreene93 <34109141+codegreene93@users.noreply.github.com>
2020-12-28 22:03:50 +0000

    publish first attempt

--- dist/config.js
+++ dist/config.js
@@ -1,9 +1,12 @@
-import dotenv from 'dotenv';
+"use strict";
 
-dotenv.config();
-
-export default {
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+var _default = {
   MONGODB_URL: process.env.MONGODB_URL || 'mongodb://localhost/honeymandesigns',
   JWT_SECRET: process.env.JWT_SECRET || 'somethingsecret',
   PAYPAL_CLIENT_ID: process.env.PAYPAL_CLIENT_ID || 'sb'
-}
+};
+exports["default"] = _default;
---
+++ dist/data.js
@@ -0,0 +1,40 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+var _default = {
+  products: [{
+    _id: '3',
+    name: 'Monkey Light',
+    category: 'lights',
+    image: '/images/monkey.jpg',
+    price: '80',
+    brand: 'Acuity',
+    rating: 4.5,
+    numReviews: 10,
+    countInStock: 8
+  }, {
+    _id: '2',
+    name: 'Elephant Lamp',
+    category: 'lights',
+    image: '/images/monkey.jpg',
+    price: '80',
+    brand: 'Acuity',
+    rating: 4.5,
+    numReviews: 10,
+    countInStock: 9
+  }, {
+    _id: '3',
+    name: 'Decorative Lamp',
+    category: 'lights',
+    image: '/images/monkey.jpg',
+    price: '80',
+    brand: 'Acuity',
+    rating: 4.5,
+    numReviews: 10,
+    countInStock: 11
+  }]
+};
+exports["default"] = _default;
