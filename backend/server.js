1aa919dd1c5c7d4a9bf8b09fa6804328049c82ee
codegreene93 <34109141+codegreene93@users.noreply.github.com>
2020-12-30 12:25:39 +0000

    deploy

--- backend/server.js
+++ backend/server.js
@@ -21,6 +21,10 @@ app.use("/api/orders", orderRoute);
 app.get("/api/config/paypal", (req, res) => {
   res.send(config.PAYPAL_CLIENT_ID);
 })
+app.use(express.static(path.join(__dirname, '/frontend/build')));
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
+);
 
 /*app.get("/api/products/:id", (req, res) => {
    const productId = req.params.id;
@@ -33,4 +37,7 @@ app.get("/api/config/paypal", (req, res) => {
  //app.get("/api/products", (req, res) => {
   // res.send(data.products);
 // });
-app.listen(5000, () => { console.log("Server started at http://localhost:5000") });
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Serve at http://localhost:${port}`);
+});
